3fa087e11f07abcd90abaab2bfa4c9e1e1dac376
lizhgb <lizhgb@yonyou.com>
2026-08-15 13:30:13 +0800

    feat: PWA 自动升级检测，新版本就绪时 toast 提示刷新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'litools-v2';
+const CACHE = 'litools-v2.0.0';
 const ASSETS = [
   './',
   'index.html',
@@ -13,7 +13,13 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.matchAll()).then(clients => {
+      clients.forEach(c => c.postMessage({ type: 'SW_UPDATED' }));
+      return self.clients.claim();
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
